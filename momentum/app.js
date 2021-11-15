a0b7eeb601d8797bca8eeb2d5e75e79d0e7f758d
0dyuni <91406724+0dyuni@users.noreply.github.com>
2021-11-15 16:54:56 +0900

    toggle

--- momentum/app.js
+++ momentum/app.js
@@ -2,13 +2,7 @@
 const h1 = document.querySelector("#hello:nth-child(2) h1");
 
 function handleH1Click() {
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else{
-        h1.classList.add(clickedClass);
-    }
-     
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleH1Click);
